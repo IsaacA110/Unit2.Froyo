d59951f2dce3b78a0527923cb0b13a66ee757797
IsaacA110 <isaacaranzaso@gmail.com>
2024-06-09 20:06:54 -0400

    versionA

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+const userInputString = prompt(
+  "Enter the froyo flavors you would like to order. Be sure to seperate each flavor with a comma.",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
